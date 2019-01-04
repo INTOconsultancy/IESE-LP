f7eb4ffcc2ee07092d662af056befbf157547a62
Erik Adrian Durán <daftpunkguy11@gmail.com>
2019-01-04 13:13:50 -0600

    Animaciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,12 @@
-$(document).ready(function () {
-    $('.slide-testimonio').slick({
+jQuery(document).ready(function () {
+    jQuery('.slide-testimonio').slick({
         dots: false,
         infinite: true,
         speed: 300,
         arrows: true,
         slidesToShow: 3,
         slidesToScroll: 3,
-        responsive: [
-            {
+        responsive: [{
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 2,
