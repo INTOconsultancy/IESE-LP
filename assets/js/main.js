c80b3e664a4a5d47cd3b3fba243860e0c12496fb
Erik Adrian Durán <daftpunkguy11@gmail.com>
2019-01-22 18:15:12 -0600

    Ajustes de modal y slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,15 @@ jQuery(document).ready(function () {
         infinite: true,
         speed: 300,
         arrows: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         responsive: [{
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2,
+                    slidesToScroll: 1,
                     infinite: true,
                 }
             },
