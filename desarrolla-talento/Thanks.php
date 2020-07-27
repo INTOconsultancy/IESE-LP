092df1890e44ad24607e4652f574bd9a7b255d07
Oscar Sánchez <oscar.sanjim@gmail.com>
2020-07-27 18:51:25 -0500

    Autodownload brochure

--- desarrolla-talento/Thanks.php
+++ desarrolla-talento/Thanks.php
@@ -14,10 +14,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!--
+
     <meta http-equiv="refresh" CONTENT="1;URL= http://bit.ly/2Ehoosi">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    -->
 
     <title>¡Gracias por tu interés!</title>
     <link rel="stylesheet" href="../assets/scss/stylesheet.css">
